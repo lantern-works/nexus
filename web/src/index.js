ea8f2adc90b152ad06fd24b87a2d1ade17970d09
West Lane <west@paperequator.com>
2018-08-25 14:49:24 -0400

    Basic Full-Page Map Display
    - use Leaflet and MapTiler

--- web/src/index.js
+++ web/src/index.js
@@ -1,1 +1,10 @@
 console.log("Hello, World");
+
+var map = L.map('map');
+L.tileLayer('https://maps.tilehosting.com/c/ade1b05a-496f-40d1-ae23-5d5aeca37da2/styles/streets/{z}/{x}/{y}.png?key=ZokpyarACItmA6NqGNhr',   {
+        attribution: false,
+        maxZoom: 16,
+        crossOrigin: true
+    } ).addTo(map);
+
+map.setView([38.42,-102.79], 4);
