f1836a38a6ef651a23edf9ad6bf10dd0df31db2d
West Lane <west@paperequator.com>
2018-08-25 14:52:35 -0400

    Switch to SVG Maps / WebGL

--- web/src/index.js
+++ web/src/index.js
@@ -1,10 +1,12 @@
 console.log("Hello, World");
 
-var map = L.map('map');
-L.tileLayer('https://maps.tilehosting.com/c/ade1b05a-496f-40d1-ae23-5d5aeca37da2/styles/streets/{z}/{x}/{y}.png?key=ZokpyarACItmA6NqGNhr',   {
-        attribution: false,
-        maxZoom: 16,
-        crossOrigin: true
-    } ).addTo(map);
+var map = L.map('map').setView([38.42,-102.79], 4);
 
-map.setView([38.42,-102.79], 4);
+var gl = L.mapboxGL({
+    attribution: false,
+    maxZoom: 16,
+    crossOrigin: true,
+    accessToken: 'not-needed',
+    style: 'https://maps.tilehosting.com/c/ade1b05a-496f-40d1-ae23-5d5aeca37da2/styles/streets/style.json?key=ZokpyarACItmA6NqGNhr'
+
+}).addTo(map);
