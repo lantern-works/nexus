f20f201c5eee668b00f33935118f8d81cc1b7316
West Lane <west@paperequator.com>
2018-08-25 15:02:16 -0400

    PouchDB
    - basic connection to lantern database with PouchDB
    - print out all venues using mango query

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,23 @@
-console.log("Hello, World");
-
+// view
 var map = L.map('map').setView([38.42,-102.79], 4);
-
 var gl = L.mapboxGL({
     attribution: false,
     maxZoom: 16,
     crossOrigin: true,
     accessToken: 'not-needed',
     style: 'https://maps.tilehosting.com/c/ade1b05a-496f-40d1-ae23-5d5aeca37da2/styles/streets/style.json?key=ZokpyarACItmA6NqGNhr'
-
 }).addTo(map);
+
+
+// model
+var db = new PouchDB("https://lantern.global/db/lnt");
+console.log(db);
+db.find({
+	selector: {
+		_id: {$gt: "v:"}
+	}
+}).then(function(result) {
+	result.docs.forEach(function(venue) {
+		console.log(venue);
+	})
+})
