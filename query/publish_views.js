d81a4619be57e6da80247af3bad5bf4b94dc44b5
West Lane <west@paperequator.com>
2018-09-25 22:22:49 -0400

    New CouchDB Views
    - add "device" view
    - add "event" view
    - also re-use code logic

--- query/publish_views.js
+++ query/publish_views.js
@@ -4,6 +4,7 @@ const log = require("simple-node-logger").createSimpleLogger({
     logFilePath: "query.log",
     dateFormat: "YYYY.MM.DD"
 });
+log.setLevel("debug");
 
 const host = "37bd9e99-2780-4965-8da8-b6b1ebb682bc-bluemix.cloudant.com"
 const cloudant_uri = "https://" + host + "/lantern-boston-flood-scenario"
@@ -11,159 +12,138 @@ var final_uri = cloudant_uri.replace("://", "://" + process.env.CLOUDANT_API_KEY
 var db = new PouchDB(final_uri); 
 
 
+
+function saveDesignDocument(design_doc)  {
+    return db.post(design_doc).then(function(response) {
+        log.info("[network] design document updated", response);
+    }).catch(function(err) {
+        log.error(err);
+    });
+}
+
+
 //--------------------------------------------------------------- Define Views
+var views = {
+    "request": {},
+    "event": {},
+    "route": {},
+    "item": {},
+    "device": {}
+}
 
-function insertItemDesignDoc(rev) {
 
 
-    var mapFn = function(doc) {
+views.item.by_geo = {
+    map: function(doc) {
         if (doc._id.substr(0,2) == "i:") {
             if (doc.hasOwnProperty("pt")) {
                 emit(doc.pt[0],doc);
             }
         }
-    }.toString();
+    }
+}
 
-
-    var design_doc = {
-        _id: "_design/item",
-        views: {
-            "by_geo": {
-                map: mapFn
+views.route.by_geo = {
+    map: function(doc) {
+        if (doc._id.substr(0,2) == "r:") {
+            if (doc.hasOwnProperty("gp")) {
+                doc.gp.forEach(function(geo) {
+                    if (geo.length > 0) {
+                        emit(geo,doc);                    
+                    }
+                });
             }
         }
     }
+}
 
-    if (rev) {
-        design_doc._rev = rev;
+
+views.device.by_geo = {
+    map: function(doc) {
+        if (doc._id.substr(0,2) == "d:") {
+            if (doc.hasOwnProperty("gp")) {
+                doc.gp.forEach(function(geo) {
+                    if (geo.length > 0) {
+                        emit(geo,doc);                    
+                    }
+                });
+            }
+        }
     }
-
-    console.log(design_doc)
-
-    return db.post(design_doc).then(function(response) {
-        log.debug("[network] design document updated", response);
-    }).catch(function(err) {
-        log.error(err);
-    })
 }
 
 
 
+views.request.by_geo = {
+    map: function(doc) {
+        if (doc._id.substr(0,2) == "q:") {
+            if (doc.hasOwnProperty("st")) {
+                emit(doc.st,doc);
+            }
+        }
+    }
+}
 
+views.event.by_status = {
+    map: function(doc) {
+        if (doc._id.substr(0,2) == "e:") {
+            if (doc.hasOwnProperty("st")) {
+                emit(doc.st,doc);
+            }
+        }
+    }.toString()
+};
 
-
-function insertRouteDesignDoc(rev) {
-
-
-    var mapFn = function(doc) {
-        if (doc._id.substr(0,2) == "r:") {
+views.event.by_geo = {
+    map: function(doc) {
+        if (doc._id.substr(0,2) == "e:") {
             if (doc.hasOwnProperty("gp")) {
                 emit( doc._id,doc);
             }
         }
-    }.toString();
+    }.toString()
+}
 
+var fns = [];
+
+for (var doc_id in views) {
+    console.log(doc_id)
 
     var design_doc = {
-        _id: "_design/route",
-        views: {
-            "by_geo": {
-                map: mapFn
+        _id: "_design/" + doc_id,
+        views: {}
+    }
+
+    for (var view_id in views[doc_id]) {
+        design_doc.views[view_id] = views[doc_id][view_id];
+        design_doc.views[view_id].map = design_doc.views[view_id].map.toString()
+    }
+
+    console.log(design_doc);
+    var fn = db.get("_design/"+doc_id, {rev_info: true})
+        .then(function(response) {
+            console.log("RESPONSE", response)
+            if (response._rev) {
+                design_doc._rev = response._rev;
+                return saveDesignDocument(design_doc);
             }
-        }
-    }
-
-    if (rev) {
-        design_doc._rev = rev;
-    }
-
-    console.log(design_doc)
-
-    return db.post(design_doc).then(function(response) {
-        log.debug("[network] design document updated", response);
-    }).catch(function(err) {
-        log.error(err);
-    })
+            else {
+                console.log(response);
+            }
+        })
+        .catch(function(err) {
+            if (err.error == "not_found") {
+                return saveDesignDocument(design_doc);
+            }
+            else {
+                log.error(err);
+            }
+        });
+    fns.push(fn);
 }
 
 
-function insertDesignDoc(rev) {
-    var mapFn = function(doc) {
-        if (doc._id.substr(0,2) == "v:") {
-            if (doc.hasOwnProperty("gp")) {
-                emit([doc.ct[0], doc.gp[0].substr(0,7), doc._id],doc);
-            }
-        }
-    }.toString();
-
-
-
-    var itemMapFn = function(doc) {
-
-        if (doc._id.substr(0,2) == "i:") {
-            if (doc.hasOwnProperty("pt")) {
-
-                doc.ct.forEach(function(category) {
-                    emit([doc.pt[0], category], doc);
-                });
-            }
-        }
-    }.toString();
-
-    var design_doc = {
-        _id: "_design/venue",
-        views: {
-            "by_geo": {
-                map: mapFn
-            },
-            "by_item": {
-                map: itemMapFn,
-                reduce: "_count"
-            }
-        }
-    }
-
-    if (rev) {
-        design_doc._rev = rev;
-    }
-
-    console.log(design_doc)
-
-    return db.post(design_doc).then(function(response) {
-        log.debug("[network] design document updated", response);
-    }).catch(function(err) {
-        log.error(err);
-    })
-}
-
-
-
-
-//--------------------------------------------------------------- Save Views
-// save design document
-db.get("_design/item", {rev_info: true}).then(function(response) {
-    insertItemDesignDoc(response._rev);
-})
-.catch(function() {
-    insertItemDesignDoc();
-})
-
-
-
-
-// save design document
-db.get("_design/route", {rev_info: true}).then(function(response) {
-    insertRouteDesignDoc(response._rev);
-})
-.catch(function() {
-    insertRouteDesignDoc();
-})
-
-
-// save design document
-db.get("_design/venue", {rev_info: true}).then(function(response) {
-    insertDesignDoc(response._rev);
-})
-.catch(function() {
-    insertDesignDoc();
-})
+Promise.all(fns).then(function() {
+    console.log("Completed View Publishing");
+    setTimeout(process.exit, 100);
+});
